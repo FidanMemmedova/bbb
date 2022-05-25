e58d6f8af29da3c066f3cf26c58c8b2e213f4644
FidanMemmedova <100687487+FidanMemmedova@users.noreply.github.com>
2022-05-25 17:22:01 +0400

    Delete fiorella_upgraded directory

--- fiorella_upgraded/fiorella/assets/js/main.js
+++
@@ -1,5 +0,0 @@
-// const hamburger_btn = document.querySelector('.hamburger');
-// const navbar = document.querySelector('.sidebar')
-// hamburger_btn.addEventListener('click', () => {
-//     navbar.classList.toggle("change");
-// });
